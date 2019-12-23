cfcbe5b439de18e4eb340c3688e8aa16c46521d0
Saulo Joab <saulojoabps@gmail.com>
2019-12-23 12:14:33 -0300

    style upgrades

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useStates, useState} from 'react';
-import {View,  StyleSheet, Dimensions, Image} from 'react-native';
+import {View,  StyleSheet, Dimensions, Image, TouchableOpacity} from 'react-native';
 import { Container, Button, Text, Item, Input, Icon, Toast, Root } from 'native-base';
 import logo from './assets/img/MONIYElogo.png';
 
@@ -34,8 +34,8 @@ export default function App(){
         <Image source={logo} style={{width: 300, height: 60}}/>
       </View>
       <View style={{textAlign: 'left', flex: 0.3, justifyContent: 'center', width: Dimensions.get('screen').width * 0.7,}}>
-        <Text style={{color: '#00BE68', fontWeight: '100', fontSize: 30, fontFamily: 'Roboto'}}>SEJA BEM-VINDO{email === 'oksaulo@gmail.com' ? ',' : ''}</Text>
-        <Text style={{color: '#00BE68', fontWeight: 'bold', fontSize: 30, fontFamily: 'Roboto'}}>{email === 'oksaulo@gmail.com' ? 'Saulo.' : 'entre abaixo.'}</Text>
+        <Text style={{color: '#00BE68', fontSize: 35, fontFamily: 'Roboto-Black'}}>SEJA BEM-VINDO</Text>
+        <Text style={{color: '#00BE68', fontSize: 30, fontFamily: 'Roboto-Thin'}}>{email === 'oksaulo@gmail.com' ? 'novamente, Saulo :)' : 'insira seus dados.'}</Text>
       </View>
       <View style={styles.formContainer}>
 
@@ -45,31 +45,35 @@ export default function App(){
               keyboardType="email-address" 
               placeholder="Seu e-mail" 
               placeholderTextColor="#329468" 
-              style={{color: '#00BE68'}}
+              style={{color: '#00BE68', fontFamily: 'Roboto-Light'}}
               onChangeText={(e) => setEmail(e)}   
             />
           </Item>
           
-          <Item style={{marginTop: 15, borderBottomColor: '#383838',}}>
+          <Item style={{marginTop: 15, borderBottomColor: '#383838'}}>
             <Icon active name='ios-lock' style={{color: "#00BE68"}} />
             <Input 
               secureTextEntry 
               placeholder="Sua senha" 
               placeholderTextColor="#329468" 
-              
-              style={{color: '#00BE68'}}
+              style={{color: '#00BE68', fontFamily: 'Roboto-Light'}}
               onChangeText={(p) => setPassword(p)}   
             />
           </Item>
 
           <Button 
           success 
-          style={{marginTop: 15, width: 100, justifyContent: 'center', alignSelf: 'center'}}
+          style={{marginTop: 30, width: 100, justifyContent: 'center', alignSelf: 'center'}}
           onPress={() => authenticate()}
-          bordered
+          
           >
             <Text>Entrar</Text>
           </Button>
+          <TouchableOpacity
+          style={{alignSelf: 'center', marginTop: 10}}
+          >
+            <Text style={{color: '#00BE68', fontFamily: 'Roboto-Light'}}>Esqueceu a senha?</Text>
+          </TouchableOpacity>
       </View>
     </Container>
     </Root>
@@ -89,6 +93,6 @@ const styles = StyleSheet.create({
   formContainer: {
     width: Dimensions.get('screen').width * 0.8,
     flex: 0.5,
-    fontFamily: 'Roboto'
+    fontFamily: 'Roboto-thin'
   }
 })
