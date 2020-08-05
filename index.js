f432765d5c10528878a1c57ddedad07e7cefbb40
saulojoab <saulojoabps@gmail.com>
2020-08-05 03:23:56 -0300

    adding buggy home screen

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-import React from 'react';
-import { AppRegistry } from "react-native";
-import { name as appName } from "./app.json";
-import { Root } from 'native-base';
-import {StatusBar} from 'react-native';
+/**
+ * @format
+ */
 
-import MainNavigator from "./screens";
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-function MainApp(){
-    return (
-    <Root>
-        <StatusBar backgroundColor="#383838" barStyle="light-content"/>
-        <MainNavigator/>
-    </Root>
-    )
-}
-
-AppRegistry.registerComponent(appName, () => MainApp);
+AppRegistry.registerComponent(appName, () => App);
